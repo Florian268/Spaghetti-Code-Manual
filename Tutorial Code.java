7685e78a834684ebd6092cf8db53ca09b1c2598f
JCP97-2024 <79323001+JCP97-2024@users.noreply.github.com>
2021-11-03 20:27:44 -0400

    File Structure Update
    Moved module1 files into module1 folder.
    Updated module1 content.

---
+++ Tutorial Code.java
@@ -0,0 +1,40 @@
+public class Tutorial
+{
+
+	//The main body of code for the program to execute
+	public static void main(String[] args){
+
+		int[] x = foo();
+
+	}
+
+	/* foo
+	* 
+	* Description:
+	* Returns an array of given size
+	* 
+	* Arguments:
+	* -int size
+	*
+	* Return
+	* -int[size] 
+	*/
+	public static int[] foo(int size){return new int[size];}
+	
+	
+	/* foo2
+	* 
+	* Description:
+	* Returns an array of given size
+	* 
+	* Arguments:
+	* -int size
+	*
+	* Return
+	* -int[size] 
+	*/
+	public static int[] foo2(int size){
+		return new int[size];
+	}
+
+}
