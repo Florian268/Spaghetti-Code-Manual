c1826ce09ab6eeb0d6f4571d62b556dfa0057e74
JCP97-2024 <79323001+JCP97-2024@users.noreply.github.com>
2021-11-05 12:58:16 -0400

    Codemirror Test File and Module 1 Content Update

--- Tutorial.java
+++ Tutorial.java
@@ -1,17 +1,17 @@
 //--------------------------------------------------------------------
-public class Tutorial{
-
-	public static void main(String[] args){
-
+public class Tutorial{											// <-- 1st Code Block
+	public static void main(String[] args){		// <-- 2nd Code Block
 		int size = 10;
 		int sum = 0;
 		int[] x = new int[size];
-	
-		for(int i=0;i<size;i++){
+		for(int i=0;i<size;i++){								// <-- 3rd Code Block
 			x[i] = i;sum = sum + i;
 		}
-
 		System.out.println("Sum is "+sum+".");}
+
+
+
+
 
 
 
