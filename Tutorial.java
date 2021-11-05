302a9402d479a36bcaccdada8e789b947ae61d33
JCP97-2024 <79323001+JCP97-2024@users.noreply.github.com>
2021-11-05 16:46:26 -0400

    Module 1 Update

--- Tutorial.java
+++ Tutorial.java
@@ -1,20 +1,42 @@
+/*
+Tutorial Program
+Author: John Pepling
+
+Description:
+	This program aims to demonstrate simple code structure practices
+	for people new to programming
+*/
+
 //--------------------------------------------------------------------
-public class Tutorial{											// <-- 1st Code Block
-	public static void main(String[] args){		// <-- 2nd Code Block
-		int size = 10;
-		int sum = 0;
-		int[] x = new int[size];
-		for(int i=0;i<size;i++){								// <-- 3rd Code Block
+public class Tutorial{
+
+	/*
+	main
+
+	Description:
+		The main body of code that a computer will execute in
+		the Java programming language.
+	*/
+	public static void main(String[] args){
+
+		// Create variables
+		int size = 10;						// Size of array x
+		int sum = 0;							// sum
+		int[] x = new int[size];	// array x with (size) elements
+
+		/*
+		places 0 in position 0, 1 in position 1, 2 in position 2, etc.
+		up to 9 in position 9
+		Sums the integers together
+		*/
+		for(int i=0;i<size;i++){
 			x[i] = i;sum = sum + i;
 		}
-		System.out.println("Sum is "+sum+".");}
 
+		// Prints the sum to the console
+		System.out.println("Sum is "+sum+".");
 
-
-
-
-
-
+	}
 
 }
 //--------------------------------------------------------------------
