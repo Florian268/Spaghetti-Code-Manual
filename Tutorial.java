744123cb4d5050b990aa15e759fd6fd9c41d6f21
JCP97-2024 <79323001+JCP97-2024@users.noreply.github.com>
2021-11-04 22:39:36 -0400

    Module 1 Update

--- Tutorial.java
+++ Tutorial.java
@@ -1,64 +1,20 @@
-public class Tutorial
-{
+//--------------------------------------------------------------------
+public class Tutorial{
 
 	public static void main(String[] args){
-		
+
 		int size = 10;
 		int sum = 0;
-		int[] x = foo(size);
-		
+		int[] x = new int[size];
+	
 		for(int i=0;i<size;i++){
-			x[i] = i;
-			sum = sum + i;
+			x[i] = i;sum = sum + i;
 		}
-		
-		System.out.println("Sum is "+sum+".");
-	}
+
+		System.out.println("Sum is "+sum+".");}
 
 
-	/* foo2
-	* 
-	* Description:
-	* Returns an array of given size
-	* 
-	* Arguments:
-	* -int size
-	*
-	* Return
-	* -int[size] 
-	*/
-	public static int[] foo2(int size){
-		return new int[size];
-	}
-	/*
-	public static int bar({
-	int one = 1;
-	int two = 2;
-	for(int i=0;i<10;i++{
-	one = one+two;
-	two = two+one;
-	return two;
-	*/
-	
-	/*
-	public static int bar(){
-	int one = 1;
-	int two = 2;
-	for(int i=0;i<10;i++{
-	one = one+two;
-	two = two+one;
-	}
-	return two;
-	}
-	*/
-	
-	public static int bar(){
-		int one = 1;
-		int two = 2;
-		for(int i=0;i<10;i++{
-			one = one+two;
-			two = two+one;
-		}
-		return two;
-	}
+
+
 }
+//--------------------------------------------------------------------
