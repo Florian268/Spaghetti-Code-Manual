e4638a33a0805b99024e696acbe9ad74b05b49ee
Jdrake4e <john4e.hohman@gmail.com>
2021-11-09 01:30:46 -0500

    Add files via upload

---
+++ module5/text_Toggle.js
@@ -0,0 +1,24 @@
+function toggleText() {
+  var text = document.getElementById("reveal1");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+function toggleText() {
+  var text = document.getElementById("reveal2");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+function toggleText() {
+  var text = document.getElementById("reveal3");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
